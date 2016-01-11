3fbe0178a28859c1e1d26562afdcd975248fb9ca
soosap <prasath.soosaithasan@gmail.com>
2016-01-11 18:54:14 +0100

    Set up react component structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/SearchBar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Youtube API Key
+const API_KEY = 'AIzaSyAX2pI3fape_2-ltCLA0I-jgThLLxgzEoU';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Step 1: Create new component
+// const is not going to change. We are never going to reassign App down the line.
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
+
+// Step 2: Take the component's generated HTML and put it on the page (i.e. the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+
