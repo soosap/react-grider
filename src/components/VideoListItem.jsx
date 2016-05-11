e554501c7f9d6464a205ed178a5cec9b68655d92
soosap <prasath.soosaithasan@gmail.com>
2016-05-11 21:18:04 +0200

    Add <VideoListItem /> component

--- src/components/VideoListItem.jsx
+++ src/components/VideoListItem.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   // "props.video" is identical to "{video}" 
   // The first object in the arguments array has a property video - assign that to "video". 
   console.log(video);
-  return (<li>Video</li>);
+
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item" onClick={() => onVideoSelect(video)}>
+      <div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">
+						{video.snippet.title}
+					</div>
+				</div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
