135727e0cc29dd6ee571b08890f8dbfc446fce8a
soosap <prasath.soosaithasan@gmail.com>
2016-05-11 21:17:25 +0200

    Update <VideoList /> component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -6,14 +6,21 @@ import VideoListItem from './VideoListItem';
 const VideoList = (props) => {
 
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
+    return (
+    <VideoListItem
+      onVideoSelect={props.onVideoSelect}
+	    key={video.etag}
+	    video={video}
+    />
+    );
   });
 
+
   return (
-    <ul className="col-md-4 list-group">
+    <ul className="col-md-12 list-group">
       {videoItems}
     </ul>
   );
-}
+};
 
 export default VideoList;
